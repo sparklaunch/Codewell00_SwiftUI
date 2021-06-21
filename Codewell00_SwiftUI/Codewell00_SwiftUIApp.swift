03f304ab3795d5de88e5d5586d7b42628028b27d
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-06-21 09:43:47 +0900

    Initial Commit

---
+++ Codewell00_SwiftUI/Codewell00_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Codewell00_SwiftUIApp.swift
+//  Codewell00_SwiftUI
+//
+//  Created by Jinwook Kim on 2021/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Codewell00_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
