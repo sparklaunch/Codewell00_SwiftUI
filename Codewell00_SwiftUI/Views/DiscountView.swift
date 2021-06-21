1ff11fca4c4256eb3ff2e9d73242b8e91489a8b2
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-06-21 09:51:49 +0900

    Add DiscountView

---
+++ Codewell00_SwiftUI/Views/DiscountView.swift
@@ -0,0 +1,34 @@
+//
+//  DiscountView.swift
+//  Codewell00_SwiftUI
+//
+//  Created by Jinwook Kim on 2021/06/21.
+//
+
+import SwiftUI
+
+struct DiscountView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("🎉 To celebrate Spense's launch, we will be waiving all fees for the entire month of April.")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                Text("You will be receiving 100% of the earnings. 🎉")
+                    .underline()
+                    .foregroundColor(.white)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            }
+            .padding()
+            .background(Color(.black))
+        }
+    }
+}
+
+struct DiscountView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiscountView()
+            .previewLayout(.sizeThatFits)
+    }
+}
